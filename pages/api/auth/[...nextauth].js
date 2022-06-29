7440d4c935ad6b577647bcc8d852f51e3902b39d
Gaston Roxas <gacaro51597@gmail.com>
2022-06-29 15:46:03 +0800

    configure session user to also display id

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -41,8 +41,23 @@ export default NextAuth({
           throw new Error("Invalid email or password!");
         }
 
-        return dbUser;
+        return {
+          name: dbUser.username,
+        };
       },
     }),
   ],
+  callbacks: {
+    session: async ({ session }) => {
+      if (session.user) {
+        try {
+          const userId = await User.findOne({ username: session.user.name });
+          session.user.id = userId._id.toString();
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      return session;
+    },
+  },
 });
